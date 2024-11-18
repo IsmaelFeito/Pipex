051a16e9a2eadffd9764d2156030228e5e0027a5
Ismael <isfemo15@gmail.com>
2024-11-18 14:56:48 +0100

    comm_part

---
+++ src/pipex.c
@@ -0,0 +1,54 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   pipex.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ifeito-m <ifeito-m@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/04 16:05:41 by ifeito-m          #+#    #+#             */
+/*   Updated: 2024/11/18 14:13:37 by ifeito-m         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "includes/pipex.h"
+
+int main(int argc, char **argv, char **env)
+{
+    int fd[3][2];
+    int i;
+    int pid1;
+
+    for (i-0; i<3; i++){
+        if (pipe(fd[i] < 0)){
+            return(1);
+        }
+    }
+    pid1 = fork();
+    if (pid1 <0)
+        return(2);
+    
+    if (pid1 == 0){
+        
+    }
+    // if (argc == 5)
+    // {
+    //     if(pipe == -1)//comunica entre comandos
+    //         error();
+    //     pid1 = fork();
+    //     if (fork == -1)
+    //         error();
+    //     if(pid1 == 0)//1st son exe 1st command
+    //     {
+    //         close(fd[READ_END]);
+
+    //         dup2(fd[WRITE_END], STDOUT_FILENO);
+    //         close(fd[WRITE_END]);
+            
+    //         execlp("/bin/ls", "ls", "-l", NULL);
+    //     }
+        
+    // }
+    // else
+    //     ft_putstr_fd("\033[31mError: Bad arguments\n\e[0m", 2);
+    // return (0);
+}
---
+++ src/pipex_utils.c
@@ -0,0 +1,80 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   pipex_utils.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ifeito-m <ifeito-m@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/18 14:11:49 by ifeito-m          #+#    #+#             */
+/*   Updated: 2024/11/18 14:56:14 by ifeito-m         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <../includes/pipex.h>
+
+char **search_comm_in_dirs(char ** dirs, char *command)
+{
+	char	*temp_pth;
+	char	*full_pth;
+	int		i;
+
+	i = 0;
+	while (dirs[i])
+	{
+		temp_pth = ft_strjoin(dirs[i], '/');
+		full_pth = ft_strjoin(temp_pth, command);
+		free(temp_pth);
+		if (access(full_pth, F_OK) == 0)
+			return (full_pth);
+		free(full_pth);
+		i++;
+	}
+	return (NULL);
+}
+
+char **get_path(char *comm, char **env)
+{
+	char **dirs;
+	char **full_pth;
+	int i;
+
+	i=0;
+	while (env[i] && !ft_strnstr(env[i], "PATH", 4))
+		i++;
+	if (!env[i])
+		return (NULL);
+	dirs = ft_split(env[i] + 5, ':');
+	if (!dirs)
+		return (NULL);
+	full_pth = search_comm_in_dirs(dirs, comm);
+	i = 0;
+	while (dirs[i])
+		free(dirs[i++]);
+	free(dirs);
+	return (full_pth);
+}
+
+void run_command(char *input, char **env)
+{
+	char **comm;
+	char **comm_pth;
+
+	comm = ft_split(input, ' ');
+	comm_pth = get_path(comm[0], env);
+
+	if (!comm_pth)
+	{
+		while (*comm)
+			free(*comm);
+		free(comm);
+		perror("Command not found");
+		exit(127);
+	}
+	execve(comm_pth, comm, env);
+	perror("Exec failed");
+	free(comm_pth);
+	while (*comm)
+		free(*comm++);
+		free(comm);
+		exit(1);
+}
